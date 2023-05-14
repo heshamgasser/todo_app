b2ca42679ff823196c1324a00f4601ae66b6703f
Hesham Gasser <heshamgasser32@gmail.com>
2023-05-14 22:46:16 +0300

    add firebase option

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,20 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
+import 'package:todo_app/firebase_options.dart';
 import 'package:todo_app/home_layout/home_layout.dart';
 import 'package:todo_app/provider/app_provider.dart';
 import 'package:todo_app/screens/setting_tab.dart';
 import 'package:todo_app/screens/task_tab.dart';
 import 'package:todo_app/shared/style/myThemeData.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(ChangeNotifierProvider(
       create: (context) => AppProvider(), child: TodoApp()));
 }
---
+++ lib/provider/task_provider.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import '../screens/widget/task_widget.dart';
+
+class TaskProvider extends ChangeNotifier {
+  TextEditingController taskTitle = TextEditingController();
+  TextEditingController taskdetail = TextEditingController();
+
+  List<TaskWidget> todoList = [];
+
+  void addTodoTask() {
+    String title = taskTitle.text;
+    String content = taskdetail.text;
+    taskTitle.text = '';
+    taskdetail.text = '';
+    todoList.add(TaskWidget(title));
+  }
+}
