f927d59e713e94a1e5ae11e86c00c2229f63048e
Hesham Gasser <heshamgasser32@gmail.com>
2023-05-17 19:42:48 +0300

    add task tab content

--- lib/screens/task_tab.dart
+++ lib/screens/task_tab.dart
@@ -1,12 +1,38 @@
+import 'package:calendar_timeline/calendar_timeline.dart';
 import 'package:flutter/material.dart';
+import 'package:todo_app/screens/widget/task_widget.dart';
 
 class TaskTab extends StatelessWidget {
   static const String routeName = 'Task Tab';
+  DateTime selectedDate = DateTime.now();
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.red,
+    return Column(
+      children: [
+        CalendarTimeline(
+          initialDate: selectedDate,
+          firstDate: selectedDate,
+          lastDate: DateTime.now().add(Duration(days: 30)),
+          onDateSelected: (p0) {
+            selectedDate = p0;
+          },
+          activeBackgroundDayColor: Theme.of(context).colorScheme.primary,
+          leftMargin: 20,
+          dayColor: Theme.of(context).colorScheme.surface,
+        ),
+        SizedBox(height: 20),
+        Expanded(
+          child: ListView.separated(
+              itemBuilder: (context, index) {
+                return TaskWidget('My Name Is Hesham');
+              },
+              separatorBuilder: (context, index) {
+                return SizedBox(height: 20);
+              },
+              itemCount: 10),
+        ),
+      ],
     );
   }
 }
